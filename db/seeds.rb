199ed1f7a84824179a5e33cc2d51ed1f29be39fe
Serhii T <tocarchuk@gmail.com>
2021-06-23 11:06:58 +0300

    add Message and User resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(username: "Kelley", password: "Password")
+user = User.create(username: "Lim", password: "Password")
+user = User.create(username: "Kim", password: "Password")
+user = User.create(username: "Djinnie", password: "Password")
+user = User.create(username: "Lola", password: "Password")
